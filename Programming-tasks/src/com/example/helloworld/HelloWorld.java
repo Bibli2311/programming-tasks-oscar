2f401220c126da969227116603511d2e112956bd
Oscar Vagle <oscar.vagle@statntt.no>
2023-08-15 15:47:56 +0200

    feat: read simple file line by line

--- Programming-tasks/src/com/example/helloworld/HelloWorld.java
+++ Programming-tasks/src/com/example/helloworld/HelloWorld.java
@@ -1,8 +1,32 @@
 package com.example.helloworld;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelloWorld {
-    public static void main(String[] args) {
+
+    public static String readAllLines(BufferedReader localReader) throws IOException {
+        StringBuilder content = new StringBuilder();
+        String line;
+
+        while ((line = localReader.readLine()) != null) {
+           content.append(line);
+           content.append((System.lineSeparator()));
+        }
+        return content.toString();
+    }
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("hellow world");
+
+        BufferedReader reader = new BufferedReader(new FileReader("src/com/example/resources/input.txt"));
+        try {
+            String lineContent = readAllLines(reader);
+            System.out.println(lineContent);
+        } catch(IOException ex) {
+            System.out.println(ex);
+        }
     }
 
 }
