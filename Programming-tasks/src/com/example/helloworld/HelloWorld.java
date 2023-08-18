aab67bc90e55fafb58d83f2e835c6c71b1d88e38
Oscar Vagle <oscar.vagle@statntt.no>
2023-08-18 10:43:04 +0200

    fix: fixed bug since had incorrect value in array

--- Programming-tasks/src/com/example/helloworld/HelloWorld.java
+++ Programming-tasks/src/com/example/helloworld/HelloWorld.java
@@ -8,12 +8,20 @@ import java.util.Scanner;
 public class HelloWorld {
 
     // first row
+
     public String fRow;
+    public String sRow;
+    public String tRow;
+    public String foRow;
+    public String[] allRows;
     int cases = 0;
 
     public HelloWorld() {
-        fRow = "qwertyuiop[]\\";
-
+        fRow = "1234567890-=";
+        sRow = "qwertyuiop[]\\";
+        tRow = "asdfghjkl;'";
+        foRow = "zxcvbnm,./";
+        allRows = new String[]{fRow, sRow, tRow, foRow};
     }
 
     public int charExists(String list, char myChar) {
@@ -25,6 +33,15 @@ public class HelloWorld {
         }
 
        return -1;
+    };
+
+    public int validIndex(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+           if (list[i] != -1) {
+               return i;
+           }
+        }
+        return  -1;
     };
 
 
@@ -46,14 +63,29 @@ public class HelloWorld {
        System.out.println("input: " + input);
        String result = "";
 
+       // first index is for index for first row.
+        // second index is for index for second row, etc.
+       int[] indices = {-1,-1,-1,-1};
+
        for (char c: input.toCharArray()) {
            // index for char that has been found in any of the keyboard rows
            int indexForChar = 0;
-           indexForChar = charExists(fRow, c);
-           // if a character was found and the index for the character is larger or equal to 0
-           if (indexForChar != -1 && (indexForChar-1) >= 0) {
-               result += fRow.charAt(indexForChar-1);
+
+           for (int i = 0; i < indices.length; i++) {
+               indices[i] = charExists(allRows[i], c);
            }
+           // index for correct rows that is checked for this iteration of loop
+           int correctRow =  validIndex(indices);
+
+           System.out.println("rows: " + allRows[correctRow]);
+           System.out.println("char: " + allRows[correctRow].charAt(indices[correctRow]));
+//
+//           indexForChar = charExists(sRow, c);
+//           // if a character was found and the index for the character is larger or equal to 0
+//           if (indexForChar != -1 && (indexForChar-1) >= 0) {
+//               result += sRow.charAt(indexForChar-1);
+//           }
+           
        }
 
         System.out.println(result);
